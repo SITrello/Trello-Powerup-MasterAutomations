75f84867e77ea2bae4fa8ef978bb490d52603b5b
SITrello <168481327+SITrello@users.noreply.github.com>
2024-04-30 08:00:45 -0500

    Update MasterAutomation.js

--- MasterAutomation.js
+++ MasterAutomation.js
@@ -3,30 +3,22 @@ window.TrelloPowerUp.initialize({
   'board-buttons': function(t, options){
     return [{
       icon: 'https://example.com/icon.png',
-      text: 'Enable Automations',
+      text: 'Enable All Automations',
       callback: function(t){
-        // Enable all automations for the current board
+        // Enable all available automations for the current board
         enableAllAutomations(t);
       }
     }];
   }
 });
 
-// Function to enable all automations on the current board
+// Function to enable all available automations on the current board
 function enableAllAutomations(t) {
-  // Retrieve the board ID
-  var boardId = t.getContext().board;
-
-  // Get all available Butler commands
+  // Retrieve all available Butler commands
   t.get('board', 'private', 'commands')
   .then(function(commands){
-    // Filter out only the enabled commands
-    var enabledCommands = commands.filter(function(command){
-      return command.enabled;
-    });
-
-    // Enable each enabled command on the board
-    enabledCommands.forEach(function(command) {
+    // Enable each command on the board
+    commands.forEach(function(command) {
       t.set('board', 'private', 'rules', {
         command: command.text,
         time: 'immediately'
@@ -39,9 +31,9 @@ function enableAllAutomations(t) {
       });
     });
 
-    // Inform the user that automations have been enabled
+    // Inform the user that all automations have been enabled
     t.alert({
-      message: 'Automations enabled for this board!',
+      message: 'All available automations enabled for this board!',
       display: 'info'
     });
   })
